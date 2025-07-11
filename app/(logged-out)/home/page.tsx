7e26c89e39c29c34e3eb63e1acd37cd19c0b3c44
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2025-07-11 12:23:14 +0200

    fix

--- app/(logged-out)/home/page.tsx
+++ app/(logged-out)/home/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { CheckCircle2, Circle } from 'lucide-react';
-import { motion } from 'framer-motion';
+import { motion, type Variants } from 'framer-motion';
 
 export default function HomePage() {
-  const container = {
+  const container: Variants = {
     hidden: { opacity: 0 },
     show: {
       opacity: 1,
@@ -15,9 +15,16 @@ export default function HomePage() {
     },
   };
 
-  const item = {
+  const item: Variants = {
     hidden: { y: 20, opacity: 0 },
-    show: { y: 0, opacity: 1, transition: { type: 'spring', stiffness: 300 } },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: 'spring' as const,
+        stiffness: 300,
+      },
+    },
   };
 
   return (
