b11a65996c150b7ccf644a0eef75787ec2692e59
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2025-10-01 16:18:32 +0200

    chore(migrations): remove outdated migration files and replace with new schema for organizations, teams, and tasks to improve database structure and support new features

--- lib/db/migrations/0001_public_ultimates.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "notification_settings" text;
--- lib/db/migrations/0003_tiny_corsair.sql
+++
@@ -1,3 +0,0 @@
-CREATE TYPE "public"."task_priority" AS ENUM('low', 'medium', 'high');--> statement-breakpoint
-ALTER TABLE "tasks" ALTER COLUMN "priority" SET DEFAULT 'medium'::"public"."task_priority";--> statement-breakpoint
-ALTER TABLE "tasks" ALTER COLUMN "priority" SET DATA TYPE "public"."task_priority" USING "priority"::"public"."task_priority";
