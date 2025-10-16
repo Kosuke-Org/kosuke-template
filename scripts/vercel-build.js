e287a1cb40ef485a352273c6547cf239315d8e12
nsosio <sosio.nicola94@tiscali.it>
2025-10-16 10:29:56 +0200

    🔧 (package.json): replace prebuild script to run db:migrate and remove vercel-build.js script

--- scripts/vercel-build.js
+++
@@ -1,8 +0,0 @@
-import { execSync } from 'child_process';
-
-if (process.env.VERCEL_ENV === 'preview') {
-  console.log('Running db:reset for preview deployment...');
-  execSync('pnpm run db:reset', { stdio: 'inherit' });
-} else {
-  console.log('Skipping db:reset (not a preview deployment)');
-}
