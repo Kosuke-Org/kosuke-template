aef4c2f158fbb650c4ed832f125590dca0a58258
Sergi <sergi.tantinya@gmail.com>
2025-11-24 14:25:41 +0100

    Update shadcn (#293)

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import * as React from 'react';
-import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
 import { DayButton, DayPicker, getDefaultClassNames } from 'react-day-picker';
 
+import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
+
 import { cn } from '@/lib/utils';
+
 import { Button, buttonVariants } from '@/components/ui/button';
 
 function Calendar({
